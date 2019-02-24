8f8056a0a06c8789b48708ee0590f178bc211e57
mario rodrigueza <mariorodriguezan@gmail.com>
2019-02-24 06:28:44 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,9 @@
 # Build your say_hello method here
+def say_hello(name)
+  puts "What is your name"
+  name = gets
+  if name == " "
+    puts "Hello " + name
+  else
+    puts "Hello Ruby Programmer!"
+end
